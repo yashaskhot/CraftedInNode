7f5d4a78e97eb77672ee796ea397452fd554cc8e
Yashas Khot <321yashas0028@dbit.in>
2023-06-03 23:56:13 +0530

    added file uploader

---
+++ File Uploader/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const upload = require('express-fileupload')
+
+const app = express()
+
+app.use(upload())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+    if(req.files) {
+        console.log(req.files)
+        var file = req.files.file
+        var filename = file.name
+        console.log(filename)
+        file.mv('./upload/' + filename, function(err) {
+            if(err) {
+                res.send(err)
+            } else {
+                res.send('File uploaded!')
+            }
+        })
+    }
+
+})    
+app.listen(5000)
